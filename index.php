7927481a44384b8c0591d54ed7d10e3d55587e10
unknown <lxq.2309@gmail.com>
2024-03-22 16:55:48 +0700

    `feat`: Them tinh nang Phan trang va Tim kiem

--- index.php
+++ index.php
@@ -2,7 +2,23 @@
 require_once 'connect.php';
 require 'config.php';
 
-$sql = 'SELECT * FROM articles';
+$searchText = null;
+if (isset ($_GET['search'])) {
+    $searchText = $_GET['search'];
+}
+$sql = "SELECT COUNT(*) FROM articles WHERE title LIKE '%$searchText%'";
+$result = mysqli_query($conn, $sql);
+$totalCount = mysqli_fetch_column($result);
+$pageSize = 5;
+$pageCount = ceil($totalCount / $pageSize);
+$pageNumber = 1;
+$recordInPage = 0;
+if (isset ($_GET['page'])) {
+    $pageNumber = $_GET['page'];
+}
+$offset = $pageSize * ($pageNumber - 1);
+
+$sql = "SELECT * FROM articles WHERE title LIKE '%$searchText%' LIMIT $pageSize OFFSET $offset";
 $result = mysqli_query($conn, $sql);
 ?>
 
@@ -14,7 +30,7 @@ $result = mysqli_query($conn, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php echo $WEBSITE_DESCRIPTION; ?>">
     <title>
-        <?php echo $WEBSITE_TITLE ?>
+        <?php echo ($searchText != null ? "Kết quả tìm kiếm $searchText" : "$WEBSITE_TITLE") . ($pageNumber != 1 ? " - Trang $pageNumber": '') ?>
     </title>
 
     <style>
@@ -52,6 +68,13 @@ $result = mysqli_query($conn, $sql);
         .header h1 a {
             color: black;
         }
+
+        a.active {
+            display: inline-block;
+            padding: 5px;
+            border: 1px solid blue;
+            background-color: aqua;
+        }
     </style>
 </head>
 
@@ -64,6 +87,14 @@ $result = mysqli_query($conn, $sql);
     </div>
 
     <div class="main">
+        <div class="search">
+            <form action="" method="GET">
+                <input type="text" name="search">
+                <input type="submit" value="Tìm">
+            </form>
+
+
+        </div>
         <a href="<?php echo $WEBSITE_URL . '/create.php' ?>"><button>Tạo bài đăng mới</button></a>
         <table>
             <thead>
@@ -76,6 +107,7 @@ $result = mysqli_query($conn, $sql);
             <tbody>
                 <?php if (mysqli_num_rows($result) > 0) { ?>
                     <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+                        <?php $recordInPage++ ?>
                         <tr>
                             <td>
                                 <?php echo $row['article_id'] ?>
@@ -104,6 +136,20 @@ $result = mysqli_query($conn, $sql);
                 <?php } ?>
             </tbody>
         </table>
+        <div>
+            <?php for ($i = 1; $i <= $pageCount; $i++) { ?>
+                <a class="<?php echo ($pageNumber == $i ? 'active' : '') ?>"
+                    href="<?php echo $WEBSITE_URL . '/?page=' . $i . '&search=' . $searchText ?>">
+                    <?php echo $i ?>
+                </a>
+            <?php } ?>
+        </div>
+        <div>
+            <p>Hiển thị
+                <?php echo $recordInPage ?> bản ghi trong tổng số
+                <?php echo $totalCount ?> bản ghi
+            </p>
+        </div>
     </div>
 
     <div class="footer">
